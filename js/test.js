93c83fce4d31386c0d66cc3816dd0f1d46a013aa
Jessie Dowding <jdowding@csumb.edu>
2018-01-27 02:11:10 +0000

    filter list, need to filter still

--- js/test.js
+++ js/test.js
@@ -50,89 +50,205 @@
 	     styles: styles,
 	     mapTypeControl: false
 	   });
-	  var largeInfowindow = new google.maps.InfoWindow();
+	   var largeInfowindow = new google.maps.InfoWindow();
+	   var bounds = new google.maps.LatLngBounds();
+	   var drawingManager = new google.maps.drawing.DrawingManager({
+	     drawingMode: google.maps.drawing.OverlayType.POLYGON,
+	     drawingControl: true,
+	     drawingControlOptions: {
+	       position: google.maps.ControlPosition.TOP_LEFT,
+	       drawingModes: [
+	         google.maps.drawing.OverlayType.POLYGON
+	       ]
+	     }
+	   });
 
-        // The following group uses the location array to create an array of markers on initialize.
-        for (var i = 0; i < locations.length; i++) {
-          // Get the position from the location array.
-          var position = locations[i].location;
-          var title = locations[i].title;
-          // Create a marker per location, and put into markers array.
-           var marker = new google.maps.Marker({
-            position: position,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            id: i
-          });
-          // Push the marker to our array of markers.
-          markers.push(marker);
-          // Create an onclick event to open an infowindow at each marker.
-          marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow);
-          });
-        }
-        //document.getElementById('show-listings').addEventListener('click', showListings);
-        //document.getElementById('hide-listings').addEventListener('click', hideListings);
-      }
+	   for (var i = 0; i < locations.length; i++) {
+	     var position = locations[i].location;
+	     var title = locations[i].title;
+	     var marker = new google.maps.Marker({
+	       position: position,
+	       title: title,
+	       animation: google.maps.Animation.DROP,
+	       id: i
+	     });
 
-      // This function populates the infowindow when the marker is clicked. We'll only allow
-      // one infowindow which will open at the marker that is clicked, and populate based
-      // on that markers position.
-      function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
-          infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title + '</div>');
-          infowindow.open(map, marker);
-          // Make sure the marker property is cleared if the infowindow is closed.
-          infowindow.addListener('closeclick', function() {
-            infowindow.marker = null;
-          });
-        }
-      }
+	     markers.push(marker);
+	     marker.addListener('click', function() {
+	       populateInfoWindow(this, largeInfowindow);
+	     });
+	     bounds.extend(markers[i].position);
+	   }
 
-      // This function will loop through the markers array and display them all.
-      function showListings() {
-        var bounds = new google.maps.LatLngBounds();
-        // Extend the boundaries of the map for each marker and display the marker
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(map);
-          bounds.extend(markers[i].position);
-        }
-        map.fitBounds(bounds);
-      }
+	   map.fitBounds(bounds);
+	 }
 
-      // This function will loop through the listings and hide them all.
-      function hideListings() {
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(null);
-        }
-      }
-    
+	 function showListings() {
+	   var bounds = new google.maps.LatLngBounds();
+	   for (var i = 0; i < markers.length; i++) {
+	     markers[i].setMap(map);
+	     bounds.extend(markers[i].position);
+	   }
+	   map.fitBounds(bounds);
+	 }
+
+	 function populateInfoWindow(marker, infowindow) {
+	   if (infowindow.marker != marker) {
+	     infowindow.marker = marker;
+	     infowindow.setContent('<div>' + marker.title + '</div>');
+	     infowindow.open(map, marker);
+	     infowindow.addListener('closeclick', function() {
+	       infowindow.setMarker = null;
+	     });
+	     var streetViewService = new google.maps.StreetViewService();
+	     var radius = 50;
+
+	     function getStreetView(data, status) {
+	       if (status == google.maps.StreetViewStatus.OK) {
+	         var nearStreetViewLocation = data.location.latLng;
+	         var heading = google.maps.geometry.spherical.computeHeading(
+	           nearStreetViewLocation, marker.position);
+	         infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+	         var panoramaOptions = {
+	           position: nearStreetViewLocation,
+	           pov: {
+	             heading: heading,
+	             pitch: 15
+	           }
+	         };
+	         var panorama = new google.maps.StreetViewPanorama(
+	           document.getElementById('pano'), panoramaOptions);
+	       }
+	       else {
+	         infowindow.setContent('<div>' + marker.title + '</div>' +
+	           '<div>No Street View Found</div>');
+	       }
+	     }
+	     streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+	     infowindow.open(map, marker);
+	   }
+	 }
+
+	 function hideMarkers(markers) {
+	   for (var i = 0; i < markers.length; i++) {
+	     markers[i].setMap(null);
+	   }
+	 }
+
+	 function getPlacesDetails(marker, infowindow) {
+	   var service = new google.maps.places.PlacesService(map);
+	   service.getDetails({
+	     placeId: marker.id
+	   }, function(place, status) {
+	     if (status === google.maps.places.PlacesServiceStatus.OK) {
+	       infowindow.marker = marker;
+	       var innerHTML = '<div>';
+	       if (place.name) {
+	         innerHTML += '<strong>' + place.name + '</strong>';
+	       }
+	       if (place.formatted_address) {
+	         innerHTML += '<br>' + place.formatted_address;
+	       }
+	       if (place.formatted_phone_number) {
+	         innerHTML += '<br>' + place.formatted_phone_number;
+	       }
+	       if (place.opening_hours) {
+	         innerHTML += '<br><br><strong>Hours:</strong><br>' +
+	           place.opening_hours.weekday_text[0] + '<br>' +
+	           place.opening_hours.weekday_text[1] + '<br>' +
+	           place.opening_hours.weekday_text[2] + '<br>' +
+	           place.opening_hours.weekday_text[3] + '<br>' +
+	           place.opening_hours.weekday_text[4] + '<br>' +
+	           place.opening_hours.weekday_text[5] + '<br>' +
+	           place.opening_hours.weekday_text[6];
+	       }
+	       if (place.photos) {
+	         innerHTML += '<br><br><img src="' + place.photos[0].getUrl({ maxHeight: 100, maxWidth: 200 }) + '">';
+	       }
+	       innerHTML += '</div>';
+	       infowindow.setContent(innerHTML);
+	       infowindow.open(map, marker);
+	       infowindow.addListener('closeclick', function() {
+	         infowindow.marker = null;
+	       });
+	     }
+	   });
+	 }
+
+	 function createMarkersForPlaces(places) {
+	   var bounds = new google.maps.LatLngBounds();
+	   for (var i = 0; i < places.length; i++) {
+	     var place = places[i];
+	     var icon = {
+	       url: place.icon,
+	       size: new google.maps.Size(35, 35),
+	       origin: new google.maps.Point(0, 0),
+	       anchor: new google.maps.Point(15, 34),
+	       scaledSize: new google.maps.Size(25, 25)
+	     };
+	     var marker = new google.maps.Marker({
+	       map: map,
+	       icon: icon,
+	       title: place.name,
+	       position: place.geometry.location,
+	       id: place.place_id
+	     });
+	     var placeInfoWindow = new google.maps.InfoWindow();
+	     marker.addListener('click', function() {
+	       if (placeInfoWindow.marker == this) {
+	         console.log("This infowindow already is on this marker!");
+	       }
+	       else {
+	         getPlacesDetails(this, placeInfoWindow);
+	       }
+	     });
+	     placeMarkers.push(marker);
+	     if (place.geometry.viewport) {
+	       bounds.union(place.geometry.viewport);
+	     }
+	     else {
+	       bounds.extend(place.geometry.location);
+	     }
+	   }
+	   map.fitBounds(bounds);
+	 }
+// Will filter the users searches of places on map
+	 function textSearchPlaces(search) {
+	   var bounds = map.getBounds();
+	   hideMarkers(placeMarkers);
+	 }
+
 
 	 var ViewModel = function() {
-	   this.searchTerm = ko.observable();
+	   this.searchTerm = ko.observable("");
 	   initMap();
 	   this.showPlaces = function() {
 	     showListings();
 	   }
 	   this.hidePlaces = function() {
-	     hideListings();
+	     hideMarkers(markers);
 	   }
 	   this.doSearch = function() {
-	     var valueEntered = this.searchTerm();
-	     var geocoder = new google.maps.Geocoder();
-	     var autocomplete = new google.maps.places.SearchBox(valueEntered);
-	     autocomplete.bindTo('bounds', map);
-	     if (valueEntered == "") {
+	     var search = this.searchTerm();
+	     if (search == "") {
 	       window.alert('You must enter an area, or address.');
+	       return;
 	     }
 	     else {
-	       //textSearchPlaces(valueEntered);
+	       var geocoder = new google.maps.Geocoder();
+	       var searchBox = new google.maps.places.SearchBox(search);
+	       searchBox.setBounds(map.getBounds());
+	       textSearchPlaces(search);
 	     }
 	   }
-	 };
+	   this.locationsList = ko.observableArray([]);
 
-function startApp() {
-	ko.applyBindings(new ViewModel());
-}
+	   for (var i = 0; i < locations.length; i++) {
+	     var title = locations[i].title;
+	     this.locationsList.push(title);
+	   }
+	 }
+
+	 function startApp() {
+	   ko.applyBindings(new ViewModel());
+	 }
